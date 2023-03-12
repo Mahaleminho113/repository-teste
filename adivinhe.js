6152ee979be4c1c1c39d669849523f94a0b29eb4
Mahaleminho113 <125839919+Mahaleminho113@users.noreply.github.com>
2023-03-11 23:14:21 -0300

    reunião 11/03

--- adivinhe.js
+++ adivinhe.js
@@ -12,16 +12,15 @@
 
 // })
 
-//Acessando tipos de variaveis 
+//Acessando tipos de variaveis
 //array
 // const array = ["ponto0","ponto1","prapra",{objeto1: "lalala", objeto2: "blablabla"}]
 // [0,1,2,3]
 // console.log(array[3].objeto2)
 
-
 //objeto
 // const objeto = {objeto1: ["ponto0","ponto1","prapra","iuoiu"], objeto2: "tatata",objeto3: "lalala"}
-// console.log(objeto.objeto1[3]) 
+// console.log(objeto.objeto1[3])
 
 //text
 // const text = 'Matheus'
@@ -38,7 +37,7 @@
 
 //     // if(input1.value > 10){
 //     //     console.log('maior que 10')
-//     // }    
+//     // }
 //     // if(input1.value == 10){
 //     //     console.log('igual 10')
 //     // }
@@ -50,7 +49,7 @@
 //     }else{
 //         console.log('nao e maior que 10')
 //     }
-    
+
 // })
 
 // const button = document.querySelector(".test1")
@@ -68,84 +67,106 @@
 //     }
 // })
 
-const array1 = ['','','',6,'']
+const array1 = ["", "", "", 6, ""];
 const objeto1 = {
-    va1: '',
-    but: 6,
-    va2: '',
-    v21:'',
-    top: '',
-}
-const array2 = ['','','',{objeto: 6},'']
+  va1: "",
+  but: 6,
+  va2: "",
+  v21: "",
+  top: "",
+};
+const array2 = ["", "", "", { objeto: 6 }, ""];
 const objeto2 = {
-    va1: '',
-    but: ['','','',6,''],
-    va2: '',
-    v21:'',
-    top: '',
-}
-const button1=document.querySelector('.button1')
-const input=document.querySelector('.input1')
-const text =document.querySelector('.text')
-button1.addEventListener('click',function(){
-     
-    const secretoArray1 = (array1[3])
-    const secretoObjeto1 =(objeto1.but) 
-    const secretoArray2 = (array2[3].objeto)
-    const secretoObjeto2 = (objeto2.but[3]) 
+  va1: "",
+  but: ["", "", "", 6, ""],
+  va2: "",
+  v21: "",
+  top: "",
+};
+const button1 = document.querySelector(".button1");
+const input = document.querySelector(".input1");
+const text = document.querySelector(".text");
+const buttonReiniciar = document.getElementById("buttonReiniciar");
+const tentativas = document.querySelector(".tentativas");
+//
+// const arrayFrutas = ["maca", "banana"];
+// arrayFrutas.push("laranja");
+// console.log(arrayFrutas.length);
+const arrayTentativa = [];
+let contadorTentativa = 3;
+tentativas.textContent = "Tentativas: " + contadorTentativa;
 
-   console.log(secretoArray2) 
-   
-// const numeroSecreto=6
-if(input.value>secretoArray1){
- text.textContent='O Número Secreto é menor'
- }
- else if(input.value==secretoArray1){
-     alert('Parabéns! Você Acertou!')
- }
-  else{
-     text.textContent='O Número Secreto é maior'
+buttonReiniciar.addEventListener("click", function () {
+  arrayTentativa.length = 0;
+  contadorTentativa = 3;
+  tentativas.textContent = "Tentativas: " + contadorTentativa;
+});
+button1.addEventListener("click", function () {
+  const secretoArray1 = array1[3]; // igual 6 do array
+
+  arrayTentativa.push(input.value);
+  //   contadorTentativa = contadorTentativa - 1;
+  //   contadorTentativa -= 1;
+  contadorTentativa--;
+  tentativas.textContent = "Tentativas: " + contadorTentativa;
+  console.log("Tentativas: " + contadorTentativa);
+
+  console.log("tamanho do array: " + arrayTentativa.length); // imprimindo tamanho
+  console.log("arrayTentativa: " + arrayTentativa); // imprimindo array
+  if (arrayTentativa.length >= 3) {
+    alert("muitas tentativas");
+    text.textContent = "";
+  } else {
+    if (input.value > secretoArray1) {
+      text.textContent = "O Número Secreto é menor";
+    } else if (input.value == secretoArray1) {
+      alert("Parabéns! Você Acertou!");
+    } else {
+      text.textContent = "O Número Secreto é maior";
+    }
   }
 
-})
+  //   const secretoObjeto1 = objeto1.but;
+  //   const secretoArray2 = array2[3].objeto;
+  //   const secretoObjeto2 = objeto2.but[3];
+  //   console.log(secretoArray2);
+  // const numeroSecreto=6
+});
 
-const array3 = ['','','',35,'']
-const objeto3 = {
-   va1: '',
-    but: 35,
-    va2: '',
-    v21:'',
-    top: '',
-}
-const array4 = ['','','',{objeto: 35},'']
-const objeto4 = {
-   va1: '',
-    but: ['','','',35,''],
-    va2: '',
-    v21:'',
-    top: '',
-}
-const button2=document.querySelector('.button2')
-const input2=document.querySelector('.input2')
-const text2 =document.querySelector('.text2')
-button2.addEventListener('click',function(){
-     
-    const secretoArray3 = (array3[3])
-    const secretoObjeto3 =(objeto3.but) 
-    const secretoArray4 = (array4[3].objeto)
-    const secretoObjeto4 = (objeto4.but[3]) 
+// //Nivel Dificil
+// const array3 = ["", "", "", 35, ""];
+// const objeto3 = {
+//   va1: "",
+//   but: 35,
+//   va2: "",
+//   v21: "",
+//   top: "",
+// };
+// const array4 = ["", "", "", { objeto: 35 }, ""];
+// const objeto4 = {
+//   va1: "",
+//   but: ["", "", "", 35, ""],
+//   va2: "",
+//   v21: "",
+//   top: "",
+// };
+// const button2 = document.querySelector(".button2");
+// const input2 = document.querySelector(".input2");
+// const text2 = document.querySelector(".text2");
+// button2.addEventListener("click", function () {
+//   const secretoArray3 = array3[3];
+//   const secretoObjeto3 = objeto3.but;
+//   const secretoArray4 = array4[3].objeto;
+//   const secretoObjeto4 = objeto4.but[3];
 
-  // console.log(secretoArray2) 
-   
-// const numeroSecreto=6
-if(input2.value>secretoArray3){
- text2.textContent='O Número Secreto é menor'
- }
- else if(input2.value==secretoArray3){
-     alert('Parabéns! Você Acertou!')
- }
-  else{
-     text2.textContent='O Número Secreto é maior'
-  }
+//   // console.log(secretoArray2)
 
-})
+//   // const numeroSecreto=6
+//   if (input2.value > secretoArray3) {
+//     text2.textContent = "O Número Secreto é menor";
+//   } else if (input2.value == secretoArray3) {
+//     alert("Parabéns! Você Acertou!");
+//   } else {
+//     text2.textContent = "O Número Secreto é maior";
+//   }
+// });

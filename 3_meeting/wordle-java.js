0499244cd05575713148bea0e125a0813b0d1048
Mahaleminho113 <125839919+Mahaleminho113@users.noreply.github.com>
2023-04-30 22:55:51 -0300

    Exercício 1
    Exercício 1

---
+++ 3_meeting/wordle-java.js
@@ -0,0 +1,218 @@
+// variaveis ------------
+const game = document.querySelector("#game");
+const line0 = document.querySelector(".line0");
+const arrayTeclado = ["qwertyuiop", "asdfghjkl", "zxcvbnm", ["Del", "Enter"]];
+let linhaAtual = 0;
+var enter, del;
+
+// ["Del", "Enter"];
+//game ---------------
+//loop para criar as linhas -----------
+for (let countLinha = 0; countLinha < 6; countLinha++) {
+  //criando div linha--------------
+  let linha = document.createElement("div");
+  linha.className = "linha";
+  //loop para criar coluna-----------
+  for (let countColuna = 0; countColuna < 5; countColuna++) {
+    //criando div caixa--------
+    let caixa = document.createElement("div");
+    //adicionando class div Caixa---------
+    caixa.className = "caixa";
+
+    //se for a primeira coluna da primeira linha
+    //como estamos iniciando o jogo
+    //colocaremos este objeto com o fundo cinza
+    if (countLinha == 0 && countColuna == 0) {
+      caixa.classList.add("fundo");
+    }
+
+    //adicionando caixa dentro das linhas-----
+    linha.appendChild(caixa);
+  }
+  //adicionando as linhas dentro do Game---
+  game.appendChild(linha);
+}
+//variavel linha------
+const linhas = document.querySelectorAll(".linha");
+//teclado ------------
+// loop para linha do teclado
+for (
+  let countLinhaTeclado = 0;
+  countLinhaTeclado < arrayTeclado.length;
+  countLinhaTeclado++
+) {
+  //criando div linha teclado-----
+  let linha = document.createElement("div");
+  // class para linha teclado----
+  linha.className = "linhaTeclado";
+  // loop para caixas do teclado
+  for (
+    let countCaixaTeclado = 0;
+    countCaixaTeclado < arrayTeclado[countLinhaTeclado].length;
+    countCaixaTeclado++
+  ) {
+    //criando button caixa teclado----
+    let caixa = document.createElement("button");
+    //class caixa do teclado-------
+    caixa.className = "caixaTeclado";
+    //adicionando letras do array na caixa do teclado -----
+    caixa.textContent = arrayTeclado[countLinhaTeclado][countCaixaTeclado];
+
+    if (caixa.textContent == "Enter") {
+      enter = caixa;
+      caixa.style.visibility = "hidden";
+    }
+
+    if (caixa.textContent == "Del") {
+      del = caixa;
+      caixa.style.visibility = "hidden";
+    }
+
+    // //adicionando evento ao button caixa do teclado---
+    caixa.addEventListener("click", function () {
+      //   //condição para button Enter ----
+      if (linhas[linhaAtual] == linhas[6]) {
+        alert("tente Novamente");
+      }
+      if (caixa.textContent == "Enter") {
+        linhas[linhaAtual].children[4].classList.remove("fundo");
+        linhaAtual++;
+        linhas[linhaAtual].children[0].classList.add("fundo");
+        enter.style.visibility = "hidden";
+      }
+
+      //  //condiçao para button Del -----
+      else if (caixa.textContent == "Del") {
+        linhas[linhaAtual].children[4].classList.remove("fundo");
+        linhas[linhaAtual].children[0].classList.add("fundo");
+        //     //loop para pular para deletar ----
+        for (y = 4; y >= 0; y--) {
+          //       //condiçoes para o loop
+          if (linhas[linhaAtual].children[y].textContent !== "") {
+            linhas[linhaAtual].children[y].textContent = "";
+            // break;
+          }
+        }
+
+        if (y == 0) {
+          del.style.visibility = "hidden";
+        }
+      } else {
+        let x = 0;
+        //achando a coluna/caixa vazia--------
+        while (x < 5 && linhas[linhaAtual].children[x].textContent !== "") {
+          x++;
+        }
+
+        if (x < 5) {
+          del.style.visibility = "visible";
+
+          linhas[linhaAtual].children[x].textContent = caixa.textContent;
+
+          if (x == 4) {
+            enter.style.visibility = "visible";
+          }
+
+          if (x >= 0 && x < 4) {
+            linhas[linhaAtual].children[x].classList.remove("fundo");
+            linhas[linhaAtual].children[x + 1].classList.add("fundo");
+          }
+        }
+      }
+    });
+
+    //adicionando div caixa na linha------
+    linha.appendChild(caixa);
+  }
+  //adicionando as linhas no teclado --
+  teclado.appendChild(linha);
+}
+
+//teste 1
+//[div.linha, div.linha, div.linha, div.linha, div.linha, div.linha]
+// linhas[5].children[3].textContent = "a";
+
+// console.log(linhas);
+
+//teste 2
+// const linhasTeste = document.querySelector(".linha");
+// console.log(linhasTeste.children[4].textContent);
+
+//
+//
+//
+//
+//
+// controlando array com loop
+//
+//
+//
+//
+//
+//
+// const a = [
+//   "a1",
+//   "b1",
+//   "c1",
+//   "d1",
+//   [
+//     "a2",
+//     "b2",
+//     "c2",
+//     "d2",
+//     ["a3", "b3", "c3", "d3", ["1", "2", "3", "4", "5", "6", "7"]],
+//   ],
+// ];
+
+// // console.log(a[0]);
+// // console.log(a[1]);
+// // console.log(a[2]);
+// // console.log(a[3]);
+// // console.log(a[4][0]);
+// // console.log(a[4][1]);
+// // console.log(a[4][2]);
+// // console.log(a[4][3]);
+// // console.log(a[4][4][0]);
+// // console.log(a[4][4][1]);
+// // console.log(a[4][4][2]);
+// // console.log(a[4][4][3]);
+// // console.log(a[4][4][4][0]);
+// // console.log(a[4][4][4][1]);
+// // console.log(a[4][4][4][2]);
+// // console.log(a[4][4][4][3]);
+// // console.log(a[4][4][4][4]);
+// // console.log(a[4][4][4][5]);
+// // console.log(a[4][4][4][6]);
+// // for (let b = 0; b < a.length; b++) {
+// //   //   console.log(a[b]);
+// //   for (let c = 0; c < a[4].length; c++) {
+// //     // console.log(a[4][c]);
+
+// //     for (let d = 0; d < a[4][4].length; d++) {
+// //       // console.log(a[4][4][d]);
+// //       for (let e = 0; e < a[4][4][4].length; e++) {
+// //         console.log(a[b][c][d][e]);
+// //       }
+// //     }
+// //   }
+// // }
+
+// for (x = 1; x < 5; x++) {
+//   for (y = 1; y < 5; y++) {
+//     console.log("x=" + x + ", y=" + y + ", x*y = " + x + "*" + y + " " + x * y);
+//     // console.log(
+//     //   `valorX= ${x}, valorY= ${y} , valorX*valorY =  ${x} * ${y} = ${x * y}`
+//     // );
+//   }
+//   // x++;
+// }
+
+// /*
+// x=1, y=1, x*y = 1*1 = 1
+// x=1, y=2, x*y = 1*2 = 2
+// x=1, y=3, x*y = 1*3 = 3
+// x=1, y=4, x*y= 1*4 = 4
+
+// x=2, y=1, x*y = 2*1 = 2
+
+// */
---
+++ 4_meeting/script.js
@@ -0,0 +1,250 @@
+//condições if e else-----------------------------------------------------
+
+// // const dia = "sexta";
+// const dia = document.querySelector(".input");
+// const analisar = document.getElementById("button");
+// analisar.addEventListener("click", function () {
+//   //   console.log(dia);
+
+//   x = dia.value;
+//   if (x == "segunda") {
+//     console.log("planejando a semana");
+//   } else if (x == "terça") {
+//     console.log("preparando videos");
+//   } else if (x == "quarta" || x == "quinta") {
+//     console.log("folga");
+//   } else if (x == "sabado" || x == "domingo") {
+//     console.log("aproveite");
+//   } else {
+//     console.log("dia invalido");
+//   }
+// });
+
+//condições switch case -------------------------------------------------
+
+// // const dia = "sexta";
+// const dia = document.querySelector(".input");
+// const analisar = document.getElementById("button");
+// analisar.addEventListener("click", function () {
+//   //   console.log(dia);
+
+//   switch (dia.value) {
+//     case "segunda":
+//       console.log("planejando a semana");
+//       break;
+//     case "terça":
+//       console.log("preparando videos");
+//       break;
+//     case "quarta":
+//     case "quinta":
+//       console.log("folga");
+//       break;
+//     case "sabado":
+//     case "domingo":
+//       console.log("aproveite");
+//       break;
+//     default:
+//       console.log("dia invalido");
+//   }
+// });
+
+//otacelio exemplo interação array e objetos-----------------------------------------------
+
+// const dia = document.querySelector(".input");
+// const analisar = document.getElementById("button");
+// analisar.addEventListener("click", function () {
+//   var x = dia.value;
+
+//   const inventory = [
+//     { name: "segunda", atividade: "teste" },
+//     { name: "terca", atividade: "trabalhar" },
+//     { name: "quarta", atividade: "decansar" },
+//   ];
+
+//   //   function atividade(item) {
+//   //     return item.name === x;
+//   //   }
+
+//   //result = inventory.find(atividade);
+
+//   //if(result === undefined) result = 'invalido';
+
+//   const result = inventory.find(({ name }) => name === x);
+
+//   console.log(result);
+// });
+
+// // Functions ---------------------------------------------------------------
+// // Functions ---------------------------------------------------------------
+// // Functions ---------------------------------------------------------------
+
+// function logger() {
+//   console.log("My name is Jonas");
+// }
+
+// // calling / running / invoking function
+// logger();
+// logger();
+// logger();
+
+// function frutas(maca, laranja) {
+//   const juice = `Suco com ${maca} macas e ${laranja} laranja.`;
+//   return juice;
+// }
+
+// console.log(frutas(3, 5));
+
+// function frutas(maca, laranja) {
+//   const juice = `Suco com ${maca} macas e ${laranja} laranja.`;
+//   console.log(juice);
+// }
+
+// frutas(3, 5);
+
+// function frutas(maca, laranja) {
+//   const suco = `Suco com ${maca} macas e ${laranja} laranja.`;
+//   return suco;
+// }
+
+// const sucoDeMaca = frutas(5, 0);
+// console.log(sucoDeMaca);
+
+// function quadrado(a, b) {
+//   return a * a + 2 * (a * b) + b * b;
+// }
+
+// console.log(quadrado(3, 5));
+
+// console.log(quadrado(2, 4));
+
+// console.log(quadrado(7, 9));
+
+// console.log(quadrado(8, 10));
+
+// console.log(quadrado(6, 12));
+// // console.log(2 * 2);
+
+// function quadrado(a) {
+//   return a ** 2;
+// }
+
+// function somar(a, b) {
+//   return a + b;
+// }
+
+// function subtrair(a, b) {
+//   return a - b;
+// }
+
+// function dividir(a, b) {
+//   return a / b;
+// }
+
+// function equacao() {
+//   return quadrado(4);
+// }
+// console.log(equacao());
+// console.log(dividir(10, "a"));
+
+// Arrow functions -----------------------------------------------------
+
+// //funciton
+// const calcularIdade = (anoNasc) => 2023 - anoNasc;
+// //atribuindo valor da function a variavel.
+// const idade = calcularIdade(1989);
+// //imprimi valor da variavel
+// console.log(idade);
+
+//funciton
+// const calcularIdade = (anoNasc) => {
+//   return 2023 - anoNasc;
+// };
+
+// //atribuindo valor da function a variavel.
+// const idade = calcularIdade(1989);
+// //imprimi valor da variavel
+// console.log(idade);
+
+// const testeES6 = "tes";
+
+// // if inline
+// const test = testeES6 == "teste" ? "sim" : "nao";
+
+// // if (testeES6 == 'teste') {
+// //     test = 'sim'
+
+// // } else {
+// //     test = 'nao'
+// // }
+
+// console.log(test);
+
+////////////////////////////////////
+// Desafio de codificação #
+
+/*
+Matheus quer construir uma calculadora de gorjetas muito simples para sempre que for comer em um restaurante. 
+Gorjeta de 15% se o valor da conta estiver entre 50 e 300. Se o valor for diferente, a gorjeta é de 20%.
+
+1. Sua tarefa é calcular a gorjeta, dependendo do valor da conta. Crie uma variável chamada 'gorjeta' para isso. 
+Não é permitido usar uma instrução if/else 😅 (Se for mais fácil para você, você pode começar com uma instrução if/else e depois 
+tentar convertê-la em um operador ternário!)
+
+2. Imprima uma string no console contendo o valor da conta, a gorjeta e o valor final (conta + gorjeta). 
+
+Exemplo: 'A conta foi de 275, a gorjeta foi de 41,25 e o valor total foi de 316,25'
+
+DADOS DE TESTE: Teste para valores de notas 275, 40 e 430
+
+DICA: Para calcular 20% de um valor, basta multiplicá-lo por 20/100 = 0,2
+DICA: Valor X está entre 50 e 300, se for >= 50 && <= 300 😉
+
+BOA SORTE 😀
+*/
+// const valorDaconta = 5000;
+// var gorjeta;
+// if (valorDaconta >= 50 && valorDaconta <= 300) {
+//   gorjeta = (valorDaconta * 15) / 100;
+// } else {
+//   gorjeta = (valorDaconta * 20) / 100;
+// }
+// console.log(gorjeta);
+
+// const calcularGorjeta=(valorDaconta)
+
+///////////////////////////////////////
+
+// Desafio de codificação #1---------------------------------
+
+function calcMedia(G1, G2, G3) {
+  return G1 + G2 + G3 / 3;
+}
+
+const mediaGolfinhos = calcMedia(44, 23, 71);
+const mediaCoalas = calcMedia(65, 54, 49);
+console.log();
+
+if (mediaGolfinhos > 2 * mediaCoalas) {
+  console.log("Vitória dos Golfinhos " + mediaGolfinhos + " vs " + mediaCoalas);
+} else if (mediaCoalas > 2 * mediaGolfinhos) {
+  console.log("Vitoria dos Coalas" + mediaGolfinhos + " vs " + mediaCoalas);
+} else {
+  console.log("empate");
+}
+
+ 
+/////////////////////////////////////////
+
+// Desafio de codificação #2--------------------------------------------
+
+function calcTip(valorDaconta)
+if (130>= valorDaconta > 50) {
+    return valorDaconta * 0.15;
+} else {
+    return valorDaconta * 0.2;
+}
+const arrayConta = [125, 555, 44];
+const arrayGorjeta = calcTip(arrayConta(1));
+console.log(arrayGorjeta)
+    ;
+

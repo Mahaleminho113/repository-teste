aa1293787f5624966c6a90ed082ed8fdaacfe12c
Mahaleminho113 <125839919+Mahaleminho113@users.noreply.github.com>
2023-06-11 22:56:18 -0300

    trabalhando

---
+++ 8_meeting/hideki/script.js
@@ -0,0 +1,129 @@
+"use strict";
+
+// Selecting elements
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
+const diceEl = document.querySelector(".dice");
+const diceDiv = document.querySelector(".dice-div");
+
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+let scores, currentScore, activePlayer, playing;
+
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  diceDiv.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
+
+// Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  const diceObj = {
+    dice1: [4],
+    dice2: [2, 6],
+    dice3: [2, 4, 6],
+    dice4: [0, 2, 6, 8],
+    dice5: [0, 2, 4, 6, 8],
+    dice6: [0, 2, 3, 5, 6, 8],
+  };
+  if (playing) {
+    // 1. Generating a random dice roll
+    let dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Clear dice
+    while (diceDiv.firstChild) {
+      diceDiv.removeChild(diceDiv.firstChild);
+    }
+
+    // 3. Display dice
+    diceEl.classList.remove("hidden");
+    diceDiv.classList.remove("hidden");
+
+    // dice = 1;
+    diceEl.src = `dice-${dice}.png`;
+
+    //diceDiv
+    for (let i = 0; i < 9; i++) {
+      const ball = document.createElement("div");
+      // const diceSelected = `dice${dice}`;
+      const diceSelected = "dice" + dice;
+
+      if (diceObj[diceSelected].includes(i)) {
+        ball.classList.add("filled");
+      }
+      // console.log(diceSelected);
+      diceDiv.appendChild(ball);
+    }
+
+    // 3. Check for rolled 1
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+      // Finish the game
+      playing = false;
+      diceEl.classList.add("hidden");
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener("click", init);
---
+++ 9_meeting/script.js
@@ -0,0 +1,27 @@
+const obj = {
+  carro1: {
+    titulo: "carro1, turbo, diesel",
+    descricao: "carro de viado",
+    imgSRC: "./ajjdkifj",
+  },
+  carro2: {
+    titulo: "carro1, turbo, diesel",
+    descricao: "carro de viado",
+    imgSRC: "./ajjdkifj",
+  },
+};
+
+obj[carro1][titulo] = "carro1, turbo, diesel";
+/**
+ * menu
+ * titulo
+ * input + button
+ * pagina inicial com todos os carros
+ *
+ * Functions
+ *
+ * quando pesquisar carro mostra so 1 carro
+ * so mostre descricao quando apertar buttonMostrar
+ *
+ *
+ */
